ff3e1a34a06aad8c0a9d758194900e134cc089cc
Michael VERGOZ <m.vergoz@vergoz.ch>
2023-06-06 20:25:33 +0200

    Release 2.2.3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const list = require("./data/tld-list")
 const hash = require("./data/tld-hash")
 const top = require("./data/tld-top")
 
+const check = require("./src/check")
 const checkDomain = require("./src/checkDomain")
 const extractDomain = require("./src/extractDomain")
 
@@ -9,6 +10,7 @@ module.exports = {
     list,
     hash,
     top,
+    check,
     checkDomain,
     extractDomain
 }
